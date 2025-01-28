d0948b9d99fe1f5ffea0f224bcb83db2dd4d53bc
ricardojosef <ricardojcf01@gmail.com>
2025-01-28 15:25:25 -0300

    att

--- desafios/Ex04/script.js
+++ desafios/Ex04/script.js
@@ -4,8 +4,13 @@ function geratabuada(){
     let tab = document.getElementById('sel')
     if(n1.value.length == 0){
         alert('Digite um número!')
+    }else{
+        tab.innerHTML = ''
+        for(let c = 1; c<=10;c++){
+            let item = document.createElement('option')
+            item.text = n +' x '+ c + ' = ' + n*c
+           /* item.value =`tab${c}`  ---- essa linha serve apenas para outras linguagens, permitindo que você saiba qual item da tabela foi selecionado. Ex: PHP*/
+            tab.appendChild(item)
     }
-    for(){
-        
-    }
+}
 }
