888bc9af465ea3df6306c4e42cb0f54c9e2a84a7
ricardojosef <ricardojcf01@gmail.com>
2025-01-29 11:42:30 -0300

    atualização quase final

--- desafios/Ex05/script.js
+++ desafios/Ex05/script.js
@@ -1,11 +1,43 @@
-function inserir(){
-    var num = document.getElementById('num')
-    n = Number(num.value)
-    tab = document.getElementById('sel')
-    array = [n,n,n,n,n,n]
-    appendchild
-}
-
-function analisador(){
+    let num = document.getElementById('num')
+    var tab = document.getElementById('sel')
+    let valores = []
+    var res = document.querySelector('div#res')
     
+    function isNumero(n){
+        if(Number(n) >= 1 && Number(n) <=100){
+            return true
+        } else {
+            return false
+        }
+    }
+    function inLista(b,l) {
+        if (l.indexOf(Number(b)) != -1){
+            return true
+        } else {
+            return false
+        }
+ }
+
+    function inserir(){
+        if(isNumero(num.value) && !inLista(num.value,valores)) {
+        } else {
+            window.alert('Valor inválido ou ja encontrado na lista')
+        }
+                let item = document.createElement('option')
+                tab.appendChild(item)
+               item.text ="O número " + Number(num.value) + ' foi adicionado'
+                valores.push(Number(num.value))
+        
+    }
+            
+function analisador(){
+    valores.sort()
+    let maiorvalor = valores.length - 1
+    let soma = 0
+        for(let c = 0;c < valores.length; c++){
+        soma += valores[c]
+    }
+    media = soma/valores.length
+
+    res.innerHTML = "Ao todo, temos "+ valores.length + " números cadastrados <br><br> O maior valor informado foi " + valores[maiorvalor] + "<br><br> O menor valor informado foi " + valores[0] + "<br><br> A soma dos valores é " + soma + "<br><br> A média dos valores digitados é " + media
 }
--- exercicios/Aula15/ambiente.js
+++ exercicios/Aula15/ambiente.js
@@ -6,4 +6,4 @@ num.push(7)
 console.log('o ultimo valor mudou, agora o vetor é: '+ num)
 console.log('o vetor tem '+ num.length + ' elementos')
 /*num.sort: Comando que ordena em ordem crescente os elementos */
-console.log('agora estão em ordem crescente: ' +num.sort())
+console.log('agora estão em ordem crescente: '  +num.sort())
