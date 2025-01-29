5d8ea82261e40816aa0d6b97ffe2aca27bb245e7
ricardojosef <ricardojcf01@gmail.com>
2025-01-29 09:43:08 -0300

    atualização

--- desafios/Ex05/script.js
+++ desafios/Ex05/script.js
@@ -1,3 +1,11 @@
+function inserir(){
+    var num = document.getElementById('num')
+    n = Number(num.value)
+    tab = document.getElementById('sel')
+    array = [n,n,n,n,n,n]
+    appendchild
+}
+
 function analisador(){
     
 }
